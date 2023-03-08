c31a838087b97bc02859daf1563ade99172b28a4
chou1111218 <91250459+chou1111218@users.noreply.github.com>
2023-03-08 12:02:57 +0800

    Add files via upload

---
+++ week02_3_circle_sin_cos/main.cpp
@@ -0,0 +1,28 @@
+#include <GL/glut.h>
+#include<math.h>
+void myCircle(float r,float x,float y)
+{
+    glBegin(GL_POLYGON);
+    for(float a=0;a<2*3.1415926;a+=0.01){
+        glVertex2f(r*cos(a)+x,r*sin(a)+y);
+    }
+    glEnd();
+}
+void display()
+{
+    glColor3f(1,0,1);myCircle(0.6,0,0);
+    glColor3f(1,0,0);myCircle(0.3,0.5,0.5);
+    glColor3f(0,1,0);myCircle(0.3,-0.5,0.5);
+    glColor3f(1,1,0);myCircle(0.3,-0.5,-0.5);
+    glColor3f(0,0,1);myCircle(0.3,0.5,-0.5);
+
+	glutSwapBuffers();
+}
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv); //把GLUT開起來
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutCreateWindow("GLUT Shapes");//要開視窗
+	glutDisplayFunc(display);
+	glutMainLoop();
+}
